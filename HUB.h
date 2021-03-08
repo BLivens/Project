1936206ff07474e5c1c90e56f73d99ed672c6567
michel <michel.dierckx@student.uantwerpen.be>
2021-03-08 21:23:03 +0100

    Namen moeten waarschijnlijk nog naar conventies aangepast worden, de vele getters en setters dienen om de klasse zo veel mogelijk af te schermen.

--- HUB.h
+++ HUB.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include "Centrum.h"
+#include "DesignByContract.h"
 
 
 #ifndef PROJECT_HUB_H
@@ -8,10 +10,25 @@
 
 class Hub {
 public:
+    Hub();
+    int getVaccins();
+    int getInterval();
+    int getTransport();
+    int getLevering();
+    void setLevering(int aantal_vaccins);
+    void lowerVaccins(int aantal_vaccins);
+    void raiseVaccins(int aantal_vaccins);
+    void setVaccins(int aantal_vaccins);
+    void setInterval(int aantal_dagen);
+    void setTransport(int aantal_vaccins);
+    void simulateTransport(std::ostream& onStream);
+    std::vector<Centrum> centra;
+
+private:
+    int levering;
     int vaccins;
     int interval;
     int transport;
-    std::vector<Centrum> centra;
 };
 
 #endif //PROJECT_HUB_H
