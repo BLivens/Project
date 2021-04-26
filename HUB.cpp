bf3219255148ad5096938537bf95bd2be1331a54
michel <michel.dierckx@student.uantwerpen.be>
2021-04-26 15:50:56 +0200

    Importfunctie getest + transportfunctie in Hub

--- HUB.cpp
+++ HUB.cpp
@@ -145,6 +145,7 @@ void Hub::simuleerTransport(std::ostream &onStream, Centrum* centrum) {
     REQUIRE(this->properlyInitialized(), "Hub wasn't initialized when calling berekenLadingen");
     REQUIRE(centrum->properlyInitialized(), "Centrum wasn't initialized when calling simuleerTransport");
     REQUIRE(centrumVerbonden(centrum), "simuleerTransport requires centrum to be linked with Hub.");
+    /*
     // bereken aantal ladingen
     int aantal_ladingen = berekenLadingen(centrum);
     // bereken aantal vaccins
@@ -154,8 +155,48 @@ void Hub::simuleerTransport(std::ostream &onStream, Centrum* centrum) {
     // verhoog het aantal vaccins in het centrum met de correcte hoeveelheid
     centrum->setVoorraad(centrum->getVoorraad() + aantal_vaccins);
     // schrijf overzicht uit
+    */
+    int aantal_ladingen = 0;
+    int aantal_vaccins = 0;
+    for (unsigned int i = 0; i  < vaccins.size(); i++){
+        int result = 0;
+        if (centrum->getCapaciteit() < centrum->getVoorraad()){
+            result = 0;
+        }
+            // indien er wel result nodig zijn
+        else{
+            // bereken benodigde result
+            int nodige_ladingen = (centrum->getCapaciteit() - centrum->getVoorraad())/vaccins[i]->getTransport();
+            if ((centrum->getCapaciteit() - centrum->getVoorraad())%vaccins[i]->getTransport() != 0){
+                nodige_ladingen = nodige_ladingen + 1;
+            }
+            // bereken het maximaal aantal result dat het centrum kan ontvangen
+            int max_ladingen = (centrum->getCapaciteit()*2 - centrum->getVoorraad())/vaccins[i]->getTransport();
+            // bereken het aantal beschikbare result in de Hub
+            int beschikbare_ladingen = vaccins[i]->getVoorraad()/vaccins[i]->getTransport();
+            // Indien er minder result beschikbaar zijn dan geleverd moeten worden
+            if (beschikbare_ladingen<nodige_ladingen){
+                // lever alle result die in de Hub aanwezig zijn
+                result = beschikbare_ladingen;
+            }
+                // anders als de result zo groot zijn dat het max aantal result overschreden worden
+            else if (nodige_ladingen>max_ladingen){
+                // er wordt niets geleverd
+                result = 0;
+            }
+                // anders is het aantal result gelijk aan de benodigde result
+            else {
+                result = nodige_ladingen;
+            }
+        }
+        vaccins[i]->setVoorraad(vaccins[i]->getVoorraad()-result*vaccins[i]->getTransport());
+        centrum->vaccins[i].setVoorraad(centrum->vaccins[i].getVoorraad()+result*vaccins[i]->getTransport());
+        aantal_ladingen = aantal_ladingen + result;
+        aantal_vaccins = aantal_vaccins + result * vaccins[i]->getTransport();
+    }
     onStream << "Er werden " << aantal_ladingen << " ladingen ("<< aantal_vaccins <<" vaccins) getransporteerd naar " << centrum->getNaam()<< "." <<std::endl;
 }
+
 
 bool Hub::centrumVerbonden(const Centrum* centrum) const{
     REQUIRE(this->properlyInitialized(), "Hub wasn't initialized when calling centrumVerbonden");
