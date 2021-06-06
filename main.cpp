3cb4e8814c22b969718333b793facfeccd02f2ef
michel <michel.dierckx@student.uantwerpen.be>
2021-06-07 01:45:15 +0200

    opschonen main

--- main.cpp
+++ main.cpp
@@ -8,43 +8,16 @@
 int main(int argc, char **argv) {
 
     Simulatie simulatie;
-    // input
 
-    // std::ofstream myfile;
-    // myfile.open("../testInput/zzzError2.txt");
-    ProjectImporter::importProject("../testInput/illegalSimulation5.xml", std::cout, simulatie);
-    //myfile.close();
+    ProjectImporter::importProject("../testInput/input.xml", std::cout, simulatie);
 
-    //handmatige testcode voor input_2hubs_eenvoudig.xml
-    /*
-    std::cout << simulatie.hubs[0]->vaccins[0]->getType() << std::endl;
-    std::cout << simulatie.hubs[0]->vaccins[1]->getType()<< std::endl;
-    std::cout << simulatie.hubs[1]->vaccins[0]->getType()<< std::endl;
-    std::cout << simulatie.hubs[1]->vaccins[1]->getType()<< std::endl;
-
-    std::cout << (*simulatie.hubs[0]->centra[0])->getNaam() << std::endl;
-    std::cout << (*simulatie.hubs[1]->centra[0])->getNaam() << std::endl;
-    std::cout << (*simulatie.hubs[1]->centra[1])->getNaam() << std::endl;
-
-    std::cout << (*simulatie.hubs[0]->centra[0])->vaccins[0]->getType() << std::endl;
-    std::cout << (*simulatie.hubs[0]->centra[0])->vaccins[1]->getType() << std::endl;
-    std::cout << (*simulatie.hubs[0]->centra[0])->vaccins[2]->getType() << std::endl;
-
-    std::cout << (*simulatie.hubs[1]->centra[0])->vaccins[0]->getType() << std::endl;
-    std::cout << (*simulatie.hubs[1]->centra[0])->vaccins[1]->getType() << std::endl;
-    std::cout << (*simulatie.hubs[1]->centra[0])->vaccins[2]->getType() << std::endl;
-
-    std::cout << (*simulatie.hubs[1]->centra[1])->vaccins[0]->getType() << std::endl;
-    std::cout << (*simulatie.hubs[1]->centra[1])->vaccins[1]->getType() << std::endl;
-
-    */
     // simuleren
-
     simulatie.simuleren(34, std::cout);
     // simpele uitvoer
     ProjectExporter::exportProject("output.txt", std::cout, (*simulatie.hubs[0]));
     // grafische weergave
     ProjectExporter::graphic_impression("output_grafische_weergave.txt", std::cout, (*simulatie.hubs[0]));
+    // statistische weergave
     ProjectExporter::statistics("output_statistics.txt", std::cout, simulatie);
 
     return 0;
