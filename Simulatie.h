23d41936a289cb956551f4b2c7a58e8d0a93eb26
michel <michel.dierckx@student.uantwerpen.be>
2021-06-07 02:32:44 +0200

    Simulatie.h opgeschoond

--- Simulatie.h
+++ Simulatie.h
@@ -1,6 +1,3 @@
-//
-// Created by michel on 01.06.21.
-//
 
 #ifndef PROJECT_SIMULATIE_H
 #define PROJECT_SIMULATIE_H
@@ -13,12 +10,12 @@ public:
     \n ENSURE(properlyInitialized(),"constructor must end in properlyInitialized state");
     */
     Simulatie();
-    bool properlyInitialized() const;
     /**
     \n REQUIRE(this->properlyInitialized(), "Simulatie wasn't initialized when calling simuleren");
     \n REQUIRE(dagen>=0, "Dagen must be an integer greater or equal to 0.");
     */
     void simuleren(int dagen, std::ostream &onStream);
+    bool properlyInitialized() const;
     std::vector<Hub*> hubs;
     std::vector<Centrum*> centra;
     std::map<std::string, int> geleverde_vacc;
